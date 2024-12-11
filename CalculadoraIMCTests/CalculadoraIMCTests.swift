ede20b749d2bfdce1206bc68b311213ba6af735a
JkinkJS <jkinkjs24@outlook.com>
2024-12-11 16:35:45 +0100

    Initial Commit

---
+++ CalculadoraIMCTests/CalculadoraIMCTests.swift
@@ -0,0 +1,17 @@
+//
+//  CalculadoraIMCTests.swift
+//  CalculadoraIMCTests
+//
+//  Created by Tardes on 11/12/24.
+//
+
+import Testing
+@testable import CalculadoraIMC
+
+struct CalculadoraIMCTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
